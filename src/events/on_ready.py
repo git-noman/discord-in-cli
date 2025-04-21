e1b3cfca87533d8deeaf2993d7f0aa7aae34e0c9
git-noman <nomanmahar2700@gmail.com>
2025-04-22 02:13:14 +0500

    add logger & display user id in on_ready

--- src/events/on_ready.py
+++ src/events/on_ready.py
@@ -1,4 +1,16 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter(
+    "{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H-%M",
+))
+logger.addHandler(console_handler)
+
 def setup(client):
     @client.event
     async def on_ready():
-        print(f"Logged in as {client.user.name}")
+        logger.info(f"Logged in as {client.user.name} ({client.user.id})")
